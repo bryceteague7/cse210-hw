72fdb50c2cf0bb15ee2f19a31472d480e5cf3379
bryceteague7 <bryceteague7@gmail.com>
2025-11-01 22:49:06 +0200

    Update Program.cs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+         Random random = new Random();
+        
+        
+        int magicNumber = random.Next(1, 101);
+
+        int guess = 0;  
+        int attempts = 0; 
+
+        Console.WriteLine("Welcome to Guess My Number!");
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++; 
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {attempts} guesses.");
+            }
+        }
     }
 }
