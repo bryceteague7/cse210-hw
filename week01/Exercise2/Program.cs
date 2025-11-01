b382f00e25483445bf9e30b43c857c7acff59b91
bryceteague7 <bryceteague7@gmail.com>
2025-11-01 22:47:41 +0200

    Update Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+     Console.Write("Enter your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Keep trying and you'll improve.");
+        }
+
+        string sign = "";
+        if (letter != "A" && letter != "F") 
+        {
+            int lastDigit = grade % 10;
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A" && grade < 100) 
+        {
+            int lastDigit = grade % 10;
+            if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");   
     }
 }
